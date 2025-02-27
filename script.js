ca6ec0ae905d4af4354cbcac986e79ebcc90e6ee
Nataliya <antropova.n13n@gmail.com>
2025-02-27 23:40:44 +0700

    выполненны задания с 1 - 9

---
+++ script.js
@@ -0,0 +1,59 @@
+let c = 10;
+alert(c);
+c = 20;
+alert(c);
+
+//задание 2
+
+const dateIphone = 2007;
+console.log(dateIphone);
+
+//задание  3
+
+const nameCreatorJs = 'Брендан Эйх';
+console.log(nameCreatorJs);
+
+//Задание 4 Создайте две переменные 10 и 2. С помощью метода alert выведите на экран их сумму, разность , произведение и частное: 
+
+let d = 10;
+let e = 2;
+
+alert(d + e);
+alert(d - e);
+alert(d * e);
+alert(d / e);
+
+// Задание 5 Возведите 2 в 5 степень, результат записать в переменную result и вывести на экран с помощью метода alert
+
+let result = 2 ** 5;
+alert(result);
+
+//задание 6
+
+let a = 9;
+let b = 2;
+let rest = a % b;
+console.log(rest);
+
+//задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//задание 8
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+// Задание 9
+const user = {
+    name: 'Nataliya',
+    age: 18,
+    isAdmin: true,
+}
