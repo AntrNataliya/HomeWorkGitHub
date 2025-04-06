80952bb59dbf3b709b214e82328776e44ace1a42
Nataliya <antropova.n13n@gmail.com>
2025-04-06 20:41:20 +0700

    корректировка people на people1 в первом задании

--- scriptHW-2.8.js
+++ scriptHW-2.8.js
@@ -1,13 +1,13 @@
 // Задание 1
 
-const people = [
+const people1 = [
    { name: 'Глеб', age: 29 },
    { name: 'Анна', age: 17 },
    { name: 'Олег', age: 7 },
    { name: 'Оксана', age: 47 }
 ];
 
-console.log(people.sort((a, b) => a.age - b.age));
+console.log(people1.sort((a, b) => a.age - b.age));
 
 
 // Задание 2
