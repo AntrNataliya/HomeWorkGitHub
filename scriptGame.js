8f0d96884ab82dad37e88f97a75673a67a2ee1c2
Nataliya <antropova.n13n@gmail.com>
2025-04-06 23:02:24 +0700

    Реализована связь с игрой 5

--- scriptGame.js
+++ scriptGame.js
@@ -92,4 +92,55 @@ function reverseWord() {
     let reversedText = text.split('').reverse().join('');
     alert (reversedText);
 }
-// reverseWord();
+// Игра5 камень ножницы бумага
+
+function rockScissorsPaperGame() {
+    
+    const items = ['камень', 'ножницы', 'бумага'];
+    const getItems = () => Math.floor(Math.random() * items.length);
+    let playerEnter;
+    const computerChoice = items[getItems()];
+
+    switch (computerChoice) {
+        case 'камень':
+            
+            break;
+        case 'бумага':
+           
+            break;
+        default: 'ножницы'
+            break;
+    }
+    const regexp = /^[а-яА-Я]*$/;
+
+    do {
+            playerEnter = prompt("Введите, пожалуйста, любое из слов: 'камень', 'ножницы' или 'бумага'");
+        if (regexp.test(playerEnter)) {
+            if (playerEnter.toLowerCase() === computerChoice.toLowerCase()) {
+                    alert(`Выбор компьютера: ${computerChoice}`);
+                    alert(`Выбор игрока: ${playerEnter}`);
+                    alert('Ничья!');
+                } else if (playerEnter.toLowerCase() === 'бумага' && computerChoice.toLowerCase() === 'ножницы') {
+                    alert(`Выбор компьютера: ${computerChoice}`);
+                    alert(`Выбор игрока: ${playerEnter}`);
+                    alert("Выиграл компьютер!");
+
+                } else if (playerEnter.toLowerCase() === 'камень' && computerChoice.toLowerCase() === 'бумага') {
+                    alert(`Выбор компьютера: ${computerChoice}`);
+                    alert(`Player chose: ${playerEnter}`);
+                    alert("Выиграл компьютер!");
+                } else if (playerEnter.toLowerCase() === 'ножницы' && computerChoice.toLowerCase() === 'камень') {
+                    alert(`Выбор компьютера: ${computerChoice}`);
+                    alert(`Выбор игрока: ${playerEnter}`);
+                    alert("Выиграл компьютер!");
+                } else {
+                    alert(`Выбор компьютера: ${computerChoice}`);
+                    alert(`Выбор игрока: ${playerEnter}`);
+                    alert("Вы выиграли!");
+                }
+        } else {
+            alert("Вы ввели неверное слово!");
+        }
+                        
+    } while (!regexp.test(playerEnter));
+}
